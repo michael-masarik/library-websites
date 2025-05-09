0598d0f67dc71f76e3f55d8e24849bd90b3dd667
michael-masarik <masarikfamilymichael@gmail.com>
2025-05-09 16:08:17 -0500

    Set up json processer

--- create-site.py
+++ create-site.py
@@ -4,19 +4,33 @@ import os
 from notion_client import Client # type: ignore
 import requests # type: ignore
 import pprint # type: ignore
+
 #Defining basic functions
 def n_log(data):
     formatted_data = pprint.pprint(data)
     print(formatted_data)
-#Defining the variables
-site_title = "Test Site"
+
 #The next two vars are used to get the data from the request body. comment them out if test on local
-#jsonBody = os.getenv("BODY") 
-#body = json.loads(jsonBody)
+try:
+    jsonBody = os.getenv("BODY") or open("test-data.json").read()
+except FileNotFoundError:
+    raise Exception("No BODY env var and test-data.json not found.")
+body = json.loads(jsonBody)
 #Notion API Auth
 #Comment this out if you are testing locally
 notion = Client(os.getenv("AUTH_TOKEN"))
 #Get the API token from api-token.txt file if testing locally
+#JSON Processer
+def get_data_from_body(data, *keys):
+    for key in keys:
+        data = data.get(key)
+        n_log(data)
+        if data is None:
+            return None
+    return data
+#Defining the variables
+site_title = get_data_from_body(body,"body","library_name")
+
 
 
 #Creating the page

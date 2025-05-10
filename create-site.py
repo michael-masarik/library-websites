43b93aaf45d7c88a98713f617ab644c59acfcd54
michael-masarik <masarikfamilymichael@gmail.com>
2025-05-10 16:24:07 -0500

    Adding content

--- create-site.py
+++ create-site.py
@@ -18,7 +18,8 @@ except FileNotFoundError:
 body = json.loads(jsonBody)
 #Notion API Auth
 #Comment this out if you are testing locally
-notion = Client(os.getenv("AUTH_TOKEN"))
+#notion = Client(os.getenv("AUTH_TOKEN"))
+notion = Client(auth="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") 
 #Get the API token from api-token.txt file if testing locally
 #JSON Processer
 def get_data_from_body(data, *keys):
@@ -30,6 +31,7 @@ def get_data_from_body(data, *keys):
     return data
 #Defining the variables
 site_title = get_data_from_body(body,"body","library_name")
+contact_url="mailto:"+get_data_from_body(body,"body","contact_us")
 
 
 
@@ -50,10 +52,69 @@ new_site = notion.pages.create(
     
 )
 #Appending Content to the main page
-content = notion.blocks.children.append(
+#Creating th contact us db
+contact_us_db = notion.databases.create(
+    parent={"page_id": new_site["id"]},
+    title=[
+        {
+            "text": {
+                "content": "Contact Us"
+            }
+        }
+    ],
+    properties={
+        "Name": {
+            "title": {}
+        },
+        "super:Link":{
+            type: "url"
+        }
+    }
+)
+#Appending the first paragraph INCOMPLETE
+first_paragraph = notion.blocks.children.append(
     block_id=new_site["id"],
     children=[]
 )
+#Adding toggles INCOMPLETE
+first_toggles = notion.blocks.children.append(
+    block_id=new_site["id"],
+    children=[]
+)
+
+#Adding the contact us toggle
+contact_toggle = notion.blocks.children.append(
+    block_id=new_site["id"],
+    children=[
+        {
+            "type": "toggle",
+            "toggle": {
+                "text": [
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": "Contact Us"
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+)
+toggle_id = contact_toggle["results"][0]["id"]
+notion.blocks.children.append(
+    block_id=toggle_id,
+     children=[
+        {
+            "object": "block",
+            "type": "linked_database",
+            "linked_database": {
+                "database_id": contact_us_db["id"],
+            }
+        }
+    ]
+)
+
 #Adding event page
 upcomming_events = notion.pages.create(
     parent={"page_id": new_site["id"]},
@@ -68,4 +129,23 @@ upcomming_events = notion.pages.create(
         
     }
 )
+#adding contact bottons
+notion.pages.create(
+    parent={"database_id": contact_us_db["id"]},
+    properties={
+        "Name": {
+            "title": [
+                {
+                    "text": {
+                        "content": "Contact Us"
+                    }
+                }
+            ]
+        },
+        "super:Link": {
+            "url": contact_url
+        }
+    }
+)
+
 

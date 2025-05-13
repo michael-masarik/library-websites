7a85254134a660a3c9d61fae38823f80fd5772d7
michael-masarik <masarikfamilymichael@gmail.com>
2025-05-13 12:21:11 -0500

    Added PDF uploader

---
+++ update-pdf.py
@@ -0,0 +1,93 @@
+import os
+from notion_client import Client 
+import requests 
+import pprint 
+
+#Variables
+NOTION_KEY =  os.getenv("AUTH_TOKEN")
+FILE_PATH = os.getenv("FILE_URL")
+notion = Client(auth=NOTION_KEY)
+page_id = "your_page_id"
+
+#Starting the upload for the pdf
+n_upload_url = "https://api.notion.com/v1/file_uploads"
+
+payload = {
+        "filename": "events.pdf",  
+        "content_type": "file/pdf"
+    }    
+headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": f"Bearer {NOTION_KEY}",
+        "Notion-Version": "2022-06-28"
+    }
+
+response = requests.post(n_upload_url, json=payload, headers=headers)
+
+if response.status_code == 200:
+    file_id = response.json().get("id")
+    print("Upload successfuly started! Upload id: " + file_id)
+else:
+    print("Upload failed:", response.status_code, response.text)
+    file_id = None
+
+
+#Finishing the upload for the PDF    
+file_url = FILE_PATH
+response = requests.get(file_url, stream=True)
+
+if response.status_code == 200:
+    temp_file_path = "temp_image.pdf"
+    with open(temp_file_path, "wb") as f:
+        for chunk in response.iter_content(1024):
+            f.write(chunk)
+
+    with open(temp_file_path, "rb") as f:
+        files = {
+            "file": ("image.jpg", f, "file/pdf"),
+        }
+
+        url = f"https://api.notion.com/v1/file_uploads/{file_id}/send"
+        headers = {
+            "Authorization": f"Bearer {NOTION_KEY}",
+            "Notion-Version": "2022-06-28"
+        }
+
+        response = requests.post(url, headers=headers, files=files)
+        pprint.pprint(response.json)
+
+    # Delete the temporary file
+    if os.path.exists(temp_file_path):
+        os.remove(temp_file_path)
+else:
+    print("Failed to download the file:", response.status_code)
+
+
+# 1. Get blocks
+response = notion.blocks.children.list(block_id=page_id)
+blocks = response["results"]
+
+# 2. Extract block IDs
+block_ids = [block["id"] for block in blocks]
+
+# 3. Delete each block
+for block_id in block_ids:
+    notion.blocks.delete(block_id=block_id)
+    print(f"Deleted block {block_id}")
+
+# Creating PDF embed
+notion.blocks.children.append(
+    block_id=page_id,
+    children=[
+        {
+            "type": "pdf",
+            "pdf": {
+                "type": "file-upload",
+                "file-upload": {
+                    "id": file_id
+                }
+            }
+        }
+    ]
+)
